4007345dbf149144df0c9195d0f0c4204fe62509
Sophia Dell'Arciprete <sophia.dellarciprete@iqera.it>
2023-10-05 14:46:25 +0200

    Moved some things to different files and reoganized the code a bit.

--- firstDT.R
+++ firstDT.R
@@ -6,71 +6,17 @@ renv::init()
 #renv::update() #Only used if we want to update packages 
 renv::snapshot()
 
-install.packages('readxl')
-install.packages("dplyr")
-install.packages('tidyr')
-install.packages("stringr")
-install.packages('openxlsx')
-install.packages('rlang')
-install.packages("readxl")
 
-library(readxl)
-library(dplyr)
-library(tidyr)
-library(rlang)
-library(openxlsx)
-library(stringr)
-
+#Importing the libraries and functions
+source("Library.R")
 source("Functions.R")
 
-#Gets the names of the sheets, reads them, and Puts them into different DFs into one DF
 file <- 'Data/DATATAPE INVESTITORE  BCC ANNIA CUTOFF 25102022.xlsx.xlsx'
-sheets <- excel_sheets(file)
-tibble <- lapply(sheets, function(x) read_excel(file, sheet = x))
-DT <- lapply(tibble, as.data.frame)
-names(DT) <- sheets
+DT <- read_doc_and_save_df(file)
 
-#Create a DF for each sheet 
+#Create a DF for the NDG sheet
 NDGP <- DT$NDG
-LOANS_Raw <- DT$LOANS
-ASSET <- DT$ASSET
-
-
-#DEALING WITH THE LOANS DATAFRAME
-LOANS <- LOANS_Raw
-#Delete first two rows and renames the columns 
-colnames(LOANS) <- LOANS[2, ] 
-LOANS <- LOANS[-1:-2,] 
-rownames(LOANS) <- 1:nrow(LOANS)
-
-#Cleans the column names and gives all of them to the same name convention
-colnames(LOANS) <- clean_column_names(colnames(LOANS))
-
-#Rename columns to make them match the name they're supposed to have (seen on the Metadata file)
-LOANS <- LOANS %>% rename("id.bor" = ndg, "type" = type.of.credit, "gbv.original" = total.gbv, "gbv.residual" = residual.position, "principal" = gbv.capital, "interest" = gbv.interest, "expenses" = gbv.expenses, "date.status" = default.date)
-
-#Take the UTPs from the date
-LOANS$status <- LOANS$date.status
-LOANS <-  LOANS %>% relocate(status, .after = date.status)
-LOANS$status <- replace(LOANS$status, LOANS$status!="UTP", "BAD")
-
-
-LOANS <- LOANS %>%
-  mutate(
-    id.group = NA,
-    originator = NA,
-    ptf = NA,
-    cluster.ptf = NA,
-    penalties = NA,
-    date.origination = NA,
-    date.last.act = NA,
-    flag.imputed = NA
-  )
-LOANS_FROM_METADATA <- LOANS %>% select (id.bor, id.group, originator, ptf, cluster.ptf, type, status, gbv.original, gbv.residual, principal, interest, penalties, expenses, date.origination, date.status, date.last.act, flag.imputed)
-LOANS_FROM_METADATA <- LOANS_FROM_METADATA %>% mutate(across(c(id.bor, id.group, originator, ptf, cluster.ptf, type, status), as.character) )
-LOANS_FROM_METADATA <- LOANS_FROM_METADATA %>% mutate(across(c(gbv.original, gbv.residual, principal, interest, penalties, expenses), as.numeric))
-LOANS_FROM_METADATA <- LOANS_FROM_METADATA %>% mutate(across(c(date.origination, date.status, date.last.act, flag.imputed), convertToDate))
-
+LOANSP <- DT$LOANS
 
 # Create NDG_DEP and LOANS_DEP according to their functional dependencies
 new_data_frames <- dependent.Tables(LOANS, 0.45)
@@ -101,10 +47,10 @@ LOANSP <- LOANSP %>% mutate(across(c(5:7), as.numeric))
 LOANSP <- LOANSP %>% mutate_at(vars(NDG, Asset.Link, Guarantee.Amount), as.integer)
 NDG_N <- NDG_N %>% mutate_at(vars(NDG, Date.of.last.procedure), as.integer)
 
-
+# Take the Type.of.Mortgage column and split when in the same row there are values separated by +
 LOANSP <- divide_column_by_character(LOANSP, "Type.of.Mortgage", "\\+ ")
 
-# Call the function to split the values and replace NA
+# Call the function to split the values 
 NDG_N <- divide_column_by_character(NDG_N, "Judicial.Procedures.CODE", "\\+ ")
 NDG_N <- divide_column_by_character(NDG_N, "Judicial.Procedures", "/ ")
 
